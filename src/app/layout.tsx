458bc7a8e64f9f49d3eb86a775bc3ddb176afbdd
Shreeprasandh <shreeprasandh@gmail.com>
2026-07-15 23:58:38 +0530

    feat: implement ThemeProvider and wrap RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit, Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -35,7 +36,11 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${outfit.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
